bcae026c8ebd2cbd875867190e9a917a1b4d20cf
Jim Kang <jimkang@gmail.com>
2021-09-16 17:22:07 -0400

    Copy samples into fft data std::arrays.

--- Source/Vocode.h
+++ Source/Vocode.h
@@ -2,10 +2,12 @@
 
 using namespace juce;
 
-const int fftPowerOf2 = 3;
+const int fftPowerOf2 = 16;
 const int fftSize = 1 << fftPowerOf2;
+typedef std::array<float, fftSize * 2> FFTArray;
 
-static void vocodeChannel(const float *carrierPtr, const float *infoPtr, const int outLen, float *outPtr);
+static void vocodeChannel(float *carrierPtr, float *infoPtr, const int outLen, float *outPtr);
+static void getFFT(float *samplePtr, int sampleCount, FFTArray& fftData);
 
 static void vocode(AudioBuffer<float>& carrierBuffer, AudioBuffer<float>& infoBuffer, AudioBuffer<float>& outBuffer) {
   int channelCount = carrierBuffer.getNumChannels();
@@ -14,22 +16,32 @@ static void vocode(AudioBuffer<float>& carrierBuffer, AudioBuffer<float>& infoBu
     channelCount = infoBufferChannelCount;
   }
   for (int ch = 0; ch < channelCount; ++ch) {
-    const float *carrierPtr = carrierBuffer.getReadPointer(ch);
-    const float *infoPtr = infoBuffer.getReadPointer(ch);
+    float *carrierPtr = carrierBuffer.getWritePointer(ch);
+    float *infoPtr = infoBuffer.getWritePointer(ch);
     float *outPtr = outBuffer.getWritePointer(ch);
     vocodeChannel(carrierPtr, infoPtr, outBuffer.getNumSamples(), outPtr);
   }
 }
 
-static void vocodeChannel(const float *carrierPtr, const float *infoPtr, const int outLen, float *outPtr) {
+static void vocodeChannel(float *carrierPtr, float *infoPtr, int outLen, float *outPtr) {
   // Some dummy buffer writing.
-  for (int i = 0; i < outLen; ++i) {
-    outPtr[i] = i % 2 == 0 ? carrierPtr[i] : infoPtr[i];
-  }
+  //for (int i = 0; i < outLen; ++i) {
+    //outPtr[i] = i % 2 == 0 ? carrierPtr[i] : infoPtr[i];
+  //}
+  FFTArray carrierFFTData;
+  FFTArray infoFFTData;
+  getFFT(carrierPtr, outLen, carrierFFTData);
+  getFFT(infoPtr, outLen, infoFFTData);
+}
 
+static void getFFT(float *samplePtr, int sampleCount, FFTArray& fftData) {
+  std::fill(fftData.begin(), fftData.end(), 0.0f);
+  const int sampleLimit = sampleCount > fftSize ? fftSize : sampleCount;
+  for (int sampleIndex = 0; sampleIndex < sampleLimit; ++sampleIndex) {
+    fftData[sampleIndex] = samplePtr[sampleIndex];
+  }
+  std::cout << "fftData sample early: " << fftData[sampleLimit/2] << std::endl;
+  std::cout << "fftData sample late: " << fftData[sampleLimit + sampleLimit/2] << std::endl;
   dsp::WindowingFunction<float> window(fftSize, dsp::WindowingFunction<float>::hann);
   dsp::FFT fft(fftPowerOf2);
-
-  std::array<float, fftSize * 2> fftData;
-
 }
