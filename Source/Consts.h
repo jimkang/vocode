7520e89aa468b0d86dee50dcc12b127fd043d90c
Jim Kang <jimkang@gmail.com>
2021-09-17 04:13:32 -0400

    Reconstruction more or less works, if the signal is broken up into blocks.

--- Source/Consts.h
+++ Source/Consts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-const int fftPowerOf2 = 16;
+const int fftPowerOf2 = 12;
 const int fftSize = 1 << fftPowerOf2;
 // These come from Miller Puckette's timbre-stamp Pure Data patch.
 // I imagine they were determined via experimentation.
--- Source/Reconstruct.h
+++ Source/Reconstruct.h
@@ -7,6 +7,7 @@ using namespace juce;
 using namespace std;
 
 static void reconstructChannel(const float *carrierPtr, const int outLen, float *outPtr);
+static void reconstructBlock(const float *carrierPtr, float *outPtr, int outLen);
 
 static void reconstruct(AudioBuffer<float>& carrierBuffer, AudioBuffer<float>& outBuffer) {
   int channelCount = carrierBuffer.getNumChannels();
@@ -19,6 +20,23 @@ static void reconstruct(AudioBuffer<float>& carrierBuffer, AudioBuffer<float>& o
 }
 
 static void reconstructChannel(const float *carrierPtr, int outLen, float *outPtr) {
+  // We need overlap between blocks.
+  for (int i = 0; i < outLen; i += floor(fftSize/3)) {
+    int end = i + fftSize;
+    if (end > outLen) {
+      end = outLen;
+    }
+    FFTArray blockArray;
+    copy(carrierPtr + i, carrierPtr + i + fftSize, blockArray.begin());
+    FFTArray outBlockArray;
+    reconstructBlock(blockArray.data(), outBlockArray.data(), fftSize);
+    for (int j = i; j < end; ++j) {
+      outPtr[j] += outBlockArray[j - i];
+    }
+  }
+}
+
+static void reconstructBlock(const float *carrierPtr, float *outPtr, int outLen) {
   // Run a real-only FFT on carrier signal
   ComplexFFTArray carrierFFTData;
   getFFT(carrierPtr, outLen, carrierFFTData);
@@ -39,7 +57,9 @@ static void reconstructChannel(const float *carrierPtr, int outLen, float *outPt
 
   // Copy the results to the channel.
   const int sampleLimit = outLen > fftSize ? fftSize : outLen;
+  cout << "outLen: " << outLen << ", fftSize: " << fftSize << endl;
   for (int i = 0; i < sampleLimit; ++i) {
     outPtr[i] = ifftData[i];
+    //outPtr[i] = carrierFFTData[i];
   }
 }
