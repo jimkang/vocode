54ee55ff3ff8795a2054d3d294454cd47817ab67
Jim Kang <jimkang@gmail.com>
2022-03-15 17:24:04 -0400

    Add CLion stuff.

---
+++ build/JuceLibraryCode/JuceHeader.h
@@ -0,0 +1,32 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you run cmake on your
+    project - if you alter its contents, your changes may be overwritten!
+
+    This is the header file that your files should include in order to get all the
+    JUCE library headers. You should avoid including the JUCE headers directly in
+    your own source files, because that wouldn't pick up the correct configuration
+    options for your app.
+
+*/
+
+#pragma once
+
+
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_dsp/juce_dsp.h>
+#include <juce_events/juce_events.h>
+
+
+#if ! JUCE_DONT_DECLARE_PROJECTINFO
+namespace ProjectInfo
+{
+    const char* const  projectName    = "vocode";
+    const char* const  companyName    = "";
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
+}
+#endif
